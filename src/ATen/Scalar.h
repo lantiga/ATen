lantiga/ATen

commit 17757d5100f54027391c4009ee44641abe2b7fb8

Make Scalar default constructible

src/ATen/Scalar.h
@@ -12,6 +12,8 @@ namespace at {
 
 class Scalar {
 public:
+  Scalar() : Scalar(0L) {}
+
   explicit Scalar(const Tensor & t)
   : tag(Tag::HAS_t), t(t) {
     AT_ASSERT(t.dim() == 0,"Attempting to create a Scalar from a %d dim tensor",t.dim());

commit 04190e72741200614c5aa070cc594942f7f22e33

Fix default constructor build issue on macOS
Building ATen on my system (macOS 10.12.6, clang 8.1.0) fails on Scalar 
since 17757d5100f54027391c4009ee44641abe2b7fb8, with the following error:
```
/pytorch/torch/lib/ATen/../ATen/Scalar.h:16:14: error: call to constructor of
      'at::Scalar' is ambiguous
  Scalar() : Scalar(0L) {}
             ^      ~~
```
It's due to the `L` literal not leading to a suitable constructor, as generated by
`AT_FORALL_SCALAR_TYPES`. This change fixes the error.

src/ATen/Scalar.h
@@ -13,7 +13,7 @@ namespace at {
 
 class Scalar {
 public:
-  Scalar() : Scalar(0L) {}
+  Scalar() : Scalar(0LL) {}
 
   explicit Scalar(const Tensor & t)
   : tag(Tag::HAS_t), t(t) {